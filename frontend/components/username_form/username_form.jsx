dd614f2b94ba49f515903a93243506f29d823f2e
whasting <hastingswade1@gmail.com>
2017-07-27 16:01:12 -0700

    clean up unsued directories

--- frontend/components/username_form/username_form.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const UsernameForm = () => (
-  <form onSubmit={this.handleSubmit}>
-
-  </form>
-);
-
-export default UsernameForm;
--- frontend/util/posts_api_util.js
+++
@@ -1,6 +0,0 @@
-export const fetchPosts = username => (
-  $.ajax({
-    method: 'GET',
-    url: `https://www.reddit.com/user/${username}/submitted.json`
-  })
-);
